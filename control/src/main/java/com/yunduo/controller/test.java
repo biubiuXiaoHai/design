b4fe844347519b25a720e8914a1759e368d1a86b
biubiuXiaoHai <5291256+biubiuXiaoHai@user.noreply.gitee.com>
2020-01-27 19:14:46 +0800

    测试了一下关于图片的前后工作。

---
+++ control/src/main/java/com/yunduo/controller/test.java
@@ -0,0 +1,28 @@
+package com.yunduo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@RestController
+public class test {
+    @GetMapping("test")
+    public String test(MultipartFile file ){
+        if(file.isEmpty())
+        {
+            System.out.println("文件为空~");
+        }
+        File file1=new File("D://en");
+        if(!file1.exists()){
+            System.out.println("文件不存在");
+//            file1.createNewFile();
+        }else{
+            System.out.println("文件存在~");
+        }
+        return "测试成功";
+
+    }
+}
